8ee6dc85b3a054fe99c995043881b14e063f853e
yahweh90 <yahshuaball@gmail.com>
2025-09-30 22:17:13 -0400

    docs: add WARP.md; chore: align AWS region/AMI owner

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-west-2"
+  region = "us-east-1"
 }
 
 resource "random_pet" "sg" {}
@@ -42,7 +42,7 @@ data "aws_ami" "ubuntu" {
     values = ["hvm"]
   }
 
-  owners = ["099720109477"] # Canonical
+  owners = ["675105128665"] # Canonical
 }
 
 resource "aws_instance" "web" {
